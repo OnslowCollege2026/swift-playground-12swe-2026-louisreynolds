cdbdec6cc6ac74f03a21a7a143788cd97d5321b5
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-02-20 01:26:47 +0000

    parameters

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,20 +1,41 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-
+///finds the volume of a room
+///returns: volume in square meters 
+func roomvolume() -> Double {
+    6.0 * 4.5 * 4.0
+}
+///calculates and prints a room area
+/// -parameters:
+///  - length: of room in meters
+///  - width: of room in meters
+func printArea(length: Double, width: Double) {
+    print(length * width)
+}
+///calculates a room area
+/// -parameters:
+///  - length: of room in meters
+///  - width: of room in meters
+/// - returns room area in square meters 
+func area(length: Double, width: Double) -> Double {
+    return length * width
+}
+///calculates a room volume
+/// -parameters:
+///  - length: of room in meters
+///  - width: of room in meters
+///  - height of room in meters 
+/// - returns room volume in cubic meters 
+func volume(length: Double, width: Double, height: Double) -> Double {
+    return length * width * height
+}
 @main
 struct SwiftPlayground {
     static func main() {
-        print("enter room length")
-        if let userInput = readLine(), let roomlength = Double(userInput) {
-print("enter room width")
-        if let userInput = readLine(), let roomwidth = Double(userInput) {
-
-        let roomlength = 6.0
-        let roomwidth = 4.5
-        let roomheight = 2.7
-
-        let roomarea = roomlength * roomwidth
-
-        print("Room Area: \(roomarea) m²")
+        printArea(length: 6.0, width: 5.0)
+        let Area = area(length: 4.2, width: 6.7)
+        print(Area, "m²")
+        let volumeReturn = volume(length: 5.5, width: 3.5, height: 6)
+        print("volume is \(volumeReturn) m³")
     }
 }
