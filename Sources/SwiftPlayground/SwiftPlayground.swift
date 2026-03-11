4e8c8093ece20aa6daf0d1c3e2aaead5365cf237
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-03-11 21:11:59 +0000

    unfinished 2d array challenge

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,20 +1,70 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+// task 1
+func average(of temperature: [[Int]], row: Int) -> Double {
+    let selectedRow = temperature[row]
+    var sum = 0
+    for number in selectedRow {
+        sum += number
+    }
+    return Double(sum) / Double(selectedRow.count)
+
+}
+
+// task 3
+func columnTotal(in table2: [[Int]], column: Int) {
+for number in table2 {
+    
+}
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("enter room length")
-        if let userInput = readLine(), let roomlength = Double(userInput) {
-print("enter room width")
-        if let userInput = readLine(), let roomwidth = Double(userInput) {
 
-        let roomlength = 6.0
-        let roomwidth = 4.5
-        let roomheight = 2.7
+        // task 1
+        let temperature = [
+            [56, 12, 60, 12],
+            [46, 10, 42, 67],
+            [5, 1, 44, 27],
+        ]
 
-        let roomarea = roomlength * roomwidth
+        print(
+            """
 
-        print("Room Area: \(roomarea) m²")
+            \(temperature[1] [2])
+            \(temperature[2] [0])
+            \(average(of: temperature, row: 1))
+
+            """)
+
+        // task 2
+        let table = [
+            [2, 4, 6],
+            [8, 10, 12],
+            [14, 16, 18],
+        ]
+
+        var count = 0
+
+        for row in table {
+            for value in row {
+                print(value)
+                count += 1
+            }
+        }
+        print("\(count) values were printed")
+
+// task 3
+let table2 = [
+    [3, 5, 7, 9],
+    [2, 4],
+    [8, 6, 1],
+    [10]
+]
+print("what column do you want to total?")
+let columnInput = readLine()
+columnTotal(in: table2, column: columnInput)
     }
 }
