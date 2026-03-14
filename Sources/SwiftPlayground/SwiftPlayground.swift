71517effad842e2a27f52e603962c590731a32fe
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-03-14 12:08:10 +0000

    incompleted battleships2

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -2,19 +2,40 @@
 // https://docs.swift.org/swift-book
 
 
-/// Parameter:
 /// - board: The 2D grid to display.
 func printBoard(_ board: [[String]]){
-    print(board)
+    for line in board {
+print(line.joined(separator: " | " ))
+    }
 }
+
+func guessValidator()
 
 @main
 struct SwiftPlayground {
     static func main() {
         let size = 6
-var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
-var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+var ocean = Array(repeating: Array(repeating: "~", count: size), count: size) 
 
-printBoard(ocean)
+var guesses = Array(repeating: Array(repeating: "~", count: size), count: size) 
+
+
+ocean[1][3] = "S"
+ocean[2][3] = "S"
+ocean[4][0] = "S"
+ocean[5][4] = "S"
+
+print("""
+This is battle ships. You will be asked to guess where the battle ship are on the grid.
+'~' means open sea
+'X' means hit
+'0' means miss. 
+Good luck! 
+""")
+printBoard(guesses)
+
+
     }
 }
+    
+    
