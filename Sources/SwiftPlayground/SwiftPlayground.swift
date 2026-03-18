eacfbd3824ff80f65246b4df3d5fd570d9373336
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-03-18 21:38:01 +0000

    Mar 18, 2026, 9:38 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,7 +7,7 @@ func printBoard(_ board: [[String]]) {
         print(line.joined(separator: " | "))
     }
 }
-/// tests to see that the row or column number put in is valid
+/// tests to see that the row or column number put in is valid 
 /// parameter:
 /// size - size of the grid
 /// returns the valid row or column number
@@ -31,7 +31,7 @@ func guessValidator(size: Int) -> Int {
 ///
 /// Returns: The updated guesses grid after the guess is applied.
 func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
-    var updatedGuesses = guesses
+    var updatedGuesses = guesses 
 
     if guesses[row][col] != "~" {
         print("You have already guesses that spot. Please guess a different spot.")
@@ -47,16 +47,18 @@ func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) ->
         updatedGuesses[row][col] = "0"
         return updatedGuesses
     }
-}
+    }
+    
 
 @main
 struct SwiftPlayground {
     static func main() {
         let size = 6
+        let maximumGuesses = 5
         // subtract 1 from row and column guesses as 2D arrays start at 0
         let turnGuessInto2DArrayCall = 1
 
-        // creates hidden board with the battleships on it
+        // creates hidden board with the battleships on it 
         var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
         //create board that player is guessing on
         var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
@@ -76,9 +78,11 @@ struct SwiftPlayground {
             """)
         printBoard(guesses)
 
-        for   i in 1...5 {
+var progress = 1
+
+        while progress < maximumGuesses {
             print("Please guess a row number 1-\(size)")
-            let row = guessValidator(size: size) - turnGuessInto2DArrayCall
+            let row = guessValidator(size: size) - turnGuessInto2DArrayCall 
 
             print("Please guess a column number 1-\(size)")
             let column = guessValidator(size: size) - turnGuessInto2DArrayCall
@@ -89,3 +93,4 @@ struct SwiftPlayground {
         }
     }
 }
+
