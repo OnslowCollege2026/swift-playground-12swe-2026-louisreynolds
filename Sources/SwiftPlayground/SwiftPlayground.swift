0a78103bdd8de1e3b3524e0e45d882aba43fa785
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-03-21 11:18:52 +0000

    completed battleships

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,7 +7,7 @@ func printBoard(_ board: [[String]]) {
         print(line.joined(separator: " | "))
     }
 }
-/// tests to see that the row or column number put in is valid 
+/// tests to see that the row or column number put in is valid
 /// parameter:
 /// size - size of the grid
 /// returns the valid row or column number
@@ -31,7 +31,7 @@ func guessValidator(size: Int) -> Int {
 ///
 /// Returns: The updated guesses grid after the guess is applied.
 func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
-    var updatedGuesses = guesses 
+    var updatedGuesses = guesses
 
     if guesses[row][col] != "~" {
         print("You have already guesses that spot. Please guess a different spot.")
@@ -39,61 +39,131 @@ func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) ->
     }
 
     if ocean[row][col] == "S" {
-        print("Hit!")
+        print("\n Hit!")
         updatedGuesses[row][col] = "X"
         return updatedGuesses
     } else {
-        print("Miss!")
+        print("\n Miss!")
         updatedGuesses[row][col] = "0"
         return updatedGuesses
     }
+}
+
+/// Parameters:
+/// - ocean: The hidden ships grid.
+/// - guesses: The player's current guesses grid.
+///
+/// Returns: How many ships remain unhit.
+func shipsRemaining(ocean: [[String]], guesses: [[String]]) -> Int {
+    var shipCount = 0
+    for row in 0..<ocean.count {
+        for col in 0..<ocean[row].count {
+            if ocean[row][col] == "S" {
+                shipCount += 1
+            }
+        }
     }
-    
+
+    var hitCount = 0
+    for row in 0..<guesses.count {
+        for col in 0..<guesses[row].count {
+            if guesses[row][col] == "X" {
+                hitCount += 1
+            }
+        }
+    }
+    return shipCount - hitCount
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
         let size = 6
-        let maximumGuesses = 5
+        let maximumGuesses = 10
         // subtract 1 from row and column guesses as 2D arrays start at 0
         let turnGuessInto2DArrayCall = 1
+        let amountOfShips = 4
 
-        // creates hidden board with the battleships on it 
+        // creates hidden board with the battleships on it
         var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
         //create board that player is guessing on
         var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
 
-        ocean[1][3] = "S"
-        ocean[2][3] = "S"
-        ocean[4][0] = "S"
-        ocean[5][4] = "S"
+        //randomly picks the position of the ships
+        var shipsPlaced = 0
 
+    while shipsPlaced < amountOfShips {
+            let row = Int.random(in: 0...size - 1)
+            let col = Int.random(in: 0...size - 1)
+
+            if ocean[row][col] != "S" {
+                ocean[row][col] = "S"
+                shipsPlaced += 1
+            }
+        }
+printBoard(ocean)
         print(
             """
             This is battle ships. You will be asked to guess where the battle ship are on the grid.
             '~' means open sea
             'X' means hit
-            '0' means miss. 
+            '0' means miss
+            You have \(maximumGuesses) guesses to find the \(amountOfShips) ships
             Good luck! 
+
             """)
         printBoard(guesses)
 
-var progress = 0
+        var progress = 0
 
         while progress < maximumGuesses {
-        
+
             print("Please guess a row number 1-\(size)")
-            let row = guessValidator(size: size) - turnGuessInto2DArrayCall 
+            let row = guessValidator(size: size) - turnGuessInto2DArrayCall
 
             print("Please guess a column number 1-\(size)")
             let column = guessValidator(size: size) - turnGuessInto2DArrayCall
 
-            guesses = processGuess(row: row, col: column, ocean: ocean, guesses: guesses)
+            let oldGeusses = guesses
+            let newGuesses = processGuess(row: row, col: column, ocean: ocean, guesses: guesses)
 
-            printBoard(guesses)
-            print(progress)
-            progress += 1
+            printBoard(newGuesses)
+
+            //worksout the number of ships left on the grid
+            let shipsLeft = shipsRemaining(ocean: ocean, guesses: newGuesses)
+
+            // Only go to the next move if the guesses board was updated (i.e. a valid guess).
+            if oldGeusses != newGuesses {
+                progress += 1
+                guesses = newGuesses
+            }
+
+
+            //checks if player has won the game before using all their guesses 
+            if shipsRemaining(ocean: ocean, guesses: guesses) == 0 {
+                break
+            }
+
+
+            print("There are \(shipsLeft) ships remaining.")
+            print("You have \(maximumGuesses - progress) guesses left. \n")
+        }
+
+        // worksout if player wins or loses
+        if shipsRemaining(ocean: ocean, guesses: guesses) != 0 {
+            print(
+                """
+
+                You didn't find all the battleships. 
+                You lose!
+                """)
+        } else {
+            print(
+                """
+
+                You found all the battleships. 
+                You win!
+                """)
         }
     }
 }
-
