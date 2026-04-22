235261d02f0ac0bc69b3007fbe4dfe5e1c06629b
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-04-22 23:15:22 +0000

    incomplete assessment

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,20 +1,56 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+///prints the main menu and asks for a responce determining the part of the program it goes to next
+/// returns a number 1 to 6 
+func menuChoice() -> Int {
+    print(
+        """
+
+        === Kumara shop Shop ===
+        1. Add kumara to stock
+        2. Sell kumara
+        3. Show current stock
+        4. Show sales record
+        5. Show summary information
+        6. Exit
+        Choose an option:
+        """)
+
+    while true {
+        let userInput = readLine()!
+        if let userChoice = Int(userInput), (1...6).contains(userChoice) {
+            return userChoice
+        } else {
+            print("You entered: \(userInput). Please enter a number on the menu.")
+        }
+    }
+}
+
+/// validates user imput is a number and is 1 or more 
+/// returns that number
+func intImputValidator() -> Int {
+    while true {
+        let userImput = readLine()!
+        if let userchoice = Int(userImput), userchoice > 0 {
+            return userchoice
+        } else {
+            print("Invald response, please try again")
+        }
+    }
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
-        print("enter room length")
-        if let userInput = readLine(), let roomlength = Double(userInput) {
-print("enter room width")
-        if let userInput = readLine(), let roomwidth = Double(userInput) {
+        var kumaraInStock = 0
 
-        let roomlength = 6.0
-        let roomwidth = 4.5
-        let roomheight = 2.7
+// looks at what user selected from the menu and calls that part of the program 
+        while true {
+            let userChoice = menuChoice()
+            switch userChoice {
+                case 1: 
 
-        let roomarea = roomlength * roomwidth
+            }
 
-        print("Room Area: \(roomarea) m²")
+        }
     }
 }
