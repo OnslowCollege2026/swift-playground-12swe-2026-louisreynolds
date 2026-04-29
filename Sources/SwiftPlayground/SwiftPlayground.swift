de1d97c9e528e4335ae4837788f4226f28e81a97
louisreynolds <louis.reynolds@student.onslow.school.nz>
2026-04-29 23:14:22 +0000

    assessment spell 4

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,6 +1,8 @@
+import Foundation
+
 let maxKumara: Double = 50
 let kumaraPricePerKG: Double = 3.0
-let minSale: Double = 0.1
+let minKumara: Double = 0.1
 let maxBags: Int = 5000
 let maxWeightInBags: Double = 5.0
 let bagPrice: Double = 0.2
@@ -33,7 +35,7 @@ func menuChoice() -> Int {
 
 /// validates user imput is a number and is 1 or more
 /// returns that number
-func intImputValidator() -> Double {
+func ImputValidator() -> Double {
     while true {
         let userImput = readLine()!
         if let userchoice = Double(userImput), userchoice > 0 {
@@ -53,47 +55,75 @@ func addKumara(currentStock: Double, amount: Double) -> Double {
     if currentStock + amount <= maxKumara {
         print("success! You now have \(currentStock + amount) kg of kumara")
         return currentStock + amount
+    }else if amount < 0.1 {
+        print("The minimum amount of kumara to add to stock is \(minKumara)Kg")
+        return currentStock
     } else {
         print("You can not have more than 50kg of kumara in stock.")
-        print(
-            "The maximum amount of kumara you can add to the current stock is \(maxKumara-currentStock)kg"
-        )
+        print("The maximum amount of kumara you can add to the current stock is \(maxKumara-currentStock)kg")
         return currentStock
     }
 
 }
 
 func sellKumara(currentStock: Double, saleHistory: inout [[Double]]) -> Double {
-    print ("""
-    How much kumara would you like to sell? 
-    The maxium kumara to can sell is \(currentStock)Kg")
-    kumara costs $\(kumaraPricePerKG) per Kg
-    """)
+
+let amountOfKumaraToSell: Double = 0
+let amountOfBagsToSell: Int = 0
+
+    print(
+        """
+        How much kumara would you like to sell? 
+        The maxium kumara to can sell is \(currentStock)Kg")
+        kumara costs $\(kumaraPricePerKG) per Kg
+        """)
     while true {
-    let amountOfKumaraToSell = intImputValidator()
-    if amountOfKumaraToSell < minSale {
-        print("The minimum amount of kumara to purchase is \(minSale)Kg ")
+        let amountOfKumaraToSell = ImputValidator()
+        if amountOfKumaraToSell < minKumara {
+            print("The minimum amount of kumara to purchase is \(minKumara)Kg ")
         } else if amountOfKumaraToSell > currentStock {
-        print("Insuffienct stock, miximum amount of kumara to currently purchase is \(currentStock)Kg")
-        }
-        else {
-        break
+            print(
+                "Insuffienct stock, miximum amount of kumara to currently purchase is \(currentStock)Kg"
+            )
+        } else {
+            break
         }
     }
 
-        //ask of number of bags to be purchased 
-        print ("""
+
+    //ask of number of bags to be purchased
+    print(
+        """
         How many bags would you like to sell? 
         Maximum of \(maxWeightInBags)Kg of kumara per bag
         There are are a maximum of \(maxBags) bags the can be purchased in one transaction. 
         The price of a bag is $\(bagPrice)
         """)
+
+
+
+        //gets a responce from user for a amount of bags being sold
+        //can't use inputValidator as an Int has to be returned 
         while true {
-            let amountOfBagsToSell = intImputValidator()
-            if amountOfBagsToSell <  Double(maxBags) {}
+            let bags = readLine()!
+             //checks if user has asked for enough bags for the amount of kumara they're buying
+            if let amountOfBagsToSell = Int(bags), amountOfBagsToSell > 0,  amountOfBagsToSell > Int((amountOfKumaraToSell / maxWeightInBags).rounded(.up)) {
+                break 
+            } else {
+                print("Invalid responce, Please enter the amount of bags you want")
+                print("You must purchase at least \(Int((amountOfKumaraToSell / maxWeightInBags).rounded(.up))) bags")
+            } 
         }
-    }
+        print("""
+        Success!
+        Customer has purchased \(amountOfKumaraToSell)Kg's of kumara at $\(kumaraPricePerKG) per Kg, totaling $\(amountOfKumaraToSell * kumaraPricePerKG)
+        Customer has purchased \(amountOfBagsToSell) bags at $\(bagPrice), per bag totaling $\(Double(amountOfBagsToSell) * bagPrice)
+        The grand total for this transaction is $\(amountOfKumaraToSell * kumaraPricePerKG + Double(amountOfBagsToSell) * bagPrice)
+        """)
+
+        return currentStock - amountOfKumaraToSell 
     
+        }
 
 
 
@@ -111,9 +141,9 @@ struct SwiftPlayground {
             switch userChoice {
             case 1:
                 print("How many kg of kumara would you like to add?")
-                kumaraInStock = addKumara(currentStock: kumaraInStock, amount: intImputValidator())
+                kumaraInStock = addKumara(currentStock: kumaraInStock, amount: ImputValidator())
             case 2:
-                print("How much kumara would you like to sell?")
+                kumaraInStock = sellKumara(currentStock: kumaraInStock, saleHistory: &saleHistory)
 
             default:
                 break
